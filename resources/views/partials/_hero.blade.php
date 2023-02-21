b432d778a84b305adfd9af074dbf2e8b074c8393
val0s <val0s@hotmail.com>
2023-02-21 23:08:22 +0100

    Removed center register button from hero

--- resources/views/partials/_hero.blade.php
+++ resources/views/partials/_hero.blade.php
@@ -14,11 +14,11 @@
         <p class="text-2xl text-gray-200 font-bold my-4">
             Finde deinen neuen Job - oder Mitarbeiter
         </p>
-        <div>
-            <a
-                href="register.html"
-                class="inline-block border-2 border-white text-white py-2 px-4 rounded-xl uppercase mt-2 hover:text-black hover:border-black"
-            >Registriere dich, um eine Anzeige aufzugeben</a>
-        </div>
+        {{--        <div>--}}
+        {{--            <a--}}
+        {{--                href="register.html"--}}
+        {{--                class="inline-block border-2 border-white text-white py-2 px-4 rounded-xl uppercase mt-2 hover:text-black hover:border-black"--}}
+        {{--            >Registriere dich, um eine Anzeige aufzugeben</a>--}}
+        {{--        </div>--}}
     </div>
 </section>
